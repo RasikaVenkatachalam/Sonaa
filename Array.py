144007932db9da32068d0eed001f8e1219a9c377
RasikaVenkatachalam <39819710+RasikaVenkatachalam@users.noreply.github.com>
2018-07-12 23:32:54 +0530

    Update Array.py

--- Array.py
+++ Array.py
@@ -1,7 +1,7 @@
-elem = int(input())
+ele = int(input())
 n=int(input())
 arr = []
-for i in range(0, elem):
+for i in range(0, ele):
     arr.append(int(input()))
 s=0
 for i in range(arr[0],arr[n]):
